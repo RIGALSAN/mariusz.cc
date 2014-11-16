90dd6b98c74cd4afa96cc542437ad529b6eedf09
Mariusz Ciesla <mariusz@dotmariusz.net>
2014-11-16 19:17:56 +0100

    Move Bower out of source, add Bower support to Sprockets

--- config.rb
+++ config.rb
@@ -69,6 +69,12 @@ set :markdown,  :fenced_code_blocks => true,
                 :autolink => true,
                 :smartypants => true
 
+# Add Bower to Sprockets
+after_configuration do
+    @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+      sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -76,6 +82,7 @@ configure :build do
 
   # Minify Javascript on build
   activate :minify_javascript
+
 
   # Enable cache buster
   # activate :cache_buster
